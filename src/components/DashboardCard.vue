098c2ed89de7e5ff8bb5c232dbdeef8c67d9d849
Jake Nielson <jake.nielson@freestar.io>
2017-12-12 15:54:26 -0700

    Add fa arrows to card metrics

--- src/components/DashboardCard.vue
+++ src/components/DashboardCard.vue
@@ -1,13 +1,22 @@
 <template>
   <div class="dashboard-card">
     <span class="card-name">{{metrics.name}}</span>
-    <div class="card-metric" v-for="metric in metrics.data">
+    <div class="card-metric" v-for="metric in metrics.data" :key="metric">
       <div class="card-metric-left">
         <span class="card-metric-name">{{metric.name}}</span>
-        <span class="card-metric-bar" v-for="value in metric.values" :class="{'card-metric-bar-active': isLargestValue(metric, value)}" :style="{width: getLength(value)}"></span>
+        <div class="card-metric-bar-wrapper">
+          <span class="card-metric-bar" v-for="value in metric.values" :key="value" :style="{width: getLength(value)}"></span>
+        </div>
       </div>
       <div class="card-metric-right">
-        <span class="card-metric-value" v-for="value in metric.values" :class="{'card-metric-value-active': isLargestValue(metric, value)}">{{value}}</span>
+        <div class="card-metric-value-wrapper">
+          <span class="card-metric-value" v-for="value in metric.values" :key="value">{{value}}</span>
+        </div>
+        <i class="card-metric-icon fa" :class="{
+            'fa-long-arrow-up': metric.values[0] > metric.values[1],
+            'fa-long-arrow-down': metric.values[1] > metric.values[0],
+            'fa-arrows-v': metric.values[0] === metric.values[1]
+          }"></i>
       </div>
     </div>
   </div>
@@ -51,12 +60,12 @@
   .dashboard-card {
     display: flex;
     flex-direction: column;
-    height: calc(100% - 40px);
-    width: calc(100% - 20px);
+    height: calc(100% - 60px);
+    width: calc(100% - 30px);
     border-radius: 2px;
     background-color: #FFFFFF;
     box-shadow: 0 1px 2px 0 rgba(0,0,0,0.3);
-    padding: 20px 10px;
+    padding: 30px 15px;
     user-select: none;
   }
   .card-name {
@@ -74,15 +83,21 @@
     margin-top: 20px;
   }
   .card-metric-left {
-    width: calc(100% - 50px);
+    width: calc(100% - 80px);
     display: flex;
     flex-direction: column;
     margin-right: 20px;
   }
   .card-metric-right {
-    width: 50px;
+    width: 80px;
     display: flex;
+    flex-direction: row;
+  }
+  .card-metric-value-wrapper {
+    display: flex;
+    width: 70px;
     flex-direction: column;
+    text-align: right;
   }
   .card-metric-name {
     font-size: 12px;
@@ -94,20 +109,39 @@
     color: #A3ABB6;
     margin-top: 5px;
   }
+  .card-metric-bar-wrapper {
+    width: 100%;
+    height: 100%;
+  }
   .card-metric-bar {
     height: 3px;
     margin-top: 3px;
     background: #DBE0E2;
+    display: block;
   }
-  .card-metric-bar-active {
+  .card-metric-bar:first-child {
     background: #2B2C31;
   }
   .card-metric-value {
     font-size: 12px;
     line-height: 15px;
-    color: #DBE0E2;
+    color: #A3ABB6;
+    font-weight: 500;
   }
-  .card-metric-value-active {
-    color: #2B2C31;
+  .card-metric-value:first-child {
+    color: #5A677A;
+    font-size: 16px;
+  }
+  .card-metric-icon {
+    margin-top: 5px;
+    width: 5px;
+    margin-left: 15px;
+    font-size: 12px;
+  }
+  .fa-long-arrow-up {
+    color: #3ECF8E;
+  }
+  .fa-long-arrow-down {
+    color: #DBE0E2;
   }
 </style>
