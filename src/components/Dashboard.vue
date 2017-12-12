12e357c887f4cfb3d5a8e2234a85b135903cc1c6
Jake Nielson <jake.nielson@freestar.io>
2017-12-12 11:01:44 -0700

    Init prototype

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="grid">
+    <grid-layout
+      :layout="layout"
+      :col-num="12"
+      :row-height="30"
+      :is-draggable="true"
+      :is-resizable="true"
+      :vertical-compact="true"
+      :margin="[10, 10]"
+      :use-css-transforms="true"
+    >
+      <grid-item v-for="(item, index) in layout"
+                 :x="item.x"
+                 :y="item.y"
+                 :w="item.w"
+                 :h="item.h"
+                 :i="item.i"
+                 :maxH="4"
+                 :minH="4"
+                 :minW="3"
+      >
+        <dashboard-card :metrics="metrics[index]"/>
+      </grid-item>
+    </grid-layout>
+  </div>
+</template>
+
+<script>
+  import VueGridLayout from 'vue-grid-layout';
+  import DashboardCard from './DashboardCard';
+
+  const GridLayout = VueGridLayout.GridLayout;
+  const GridItem = VueGridLayout.GridItem;
+
+  export default {
+    name: 'Dashboard',
+    components: {
+      GridLayout,
+      GridItem,
+      DashboardCard
+    },
+    data () {
+      return {
+        layout: [
+          {"x":0,"y":0,"w":4,"h":4,"i":"0"},
+          {"x":4,"y":0,"w":4,"h":4,"i":"1"},
+          {"x":8,"y":0,"w":4,"h":4,"i":"2"},
+          {"x":0,"y":4,"w":3,"h":4,"i":"3"},
+          {"x":3,"y":4,"w":6,"h":4,"i":"4"},
+          {"x":9,"y":4,"w":3,"h":4,"i":"5"},
+        ],
+        metrics: [
+          {
+            i: "0",
+            name: "Ad Unit Revenue",
+            type: "bars",
+            data: [
+              {
+                name: "MRECMARF",
+                values: [1100, 1200]
+              },
+              {
+                name: "Mobile_Sticky",
+                values: [743.77, 678.32]
+              }
+            ]
+          },
+          {
+            i: "1",
+            name: "Source Impressions",
+          },
+          {
+            i: "2",
+            name: "Location CPM",
+          },
+          {
+            i: "3",
+            name: "Device Revenue",
+          },
+          {
+            i: "4",
+            name: "My Reports",
+          },
+          {
+            i: "5",
+            name: "Device Impressions",
+          },
+        ],
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .vue-grid-item {
+    background-color: pink;
+  }
+  .vue-grid-item.resizing {
+    opacity: 0;
+  }
+</style>
---
+++ src/components/DashboardCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="dashboard-card">
+    {{metrics.name}}
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'DashboardCard',
+    props: ['metrics'],
+    data () {
+      return {}
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .dashboard-card {
+    background-color: red;
+    height: 100%;
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
